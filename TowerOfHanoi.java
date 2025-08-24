0da303448c020d99bd9825be85969cd8868f1513
Mayankthakur779 <mayanksingh9424@gmail.com>
2025-08-24 22:35:41 +0530

    Add files via upload

---
+++ TowerOfHanoi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class TowerOfHanoi{
+    public static void tower(int n,String src,String helper,String dest ){
+        if(n==1){
+            System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        tower(n-1,src,dest,helper);
+        System.out.println("Transfer disk "+n+" from "+src+" to "+dest);
+        tower(n-1,helper,src,dest);
+    }
+    public static void main(String args[]){
+    int n=3; //no of disks
+    tower(n,"S","H","D");
+}
+}
