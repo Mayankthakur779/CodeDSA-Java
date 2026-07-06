6dd144e4d4dc3cac873a22123d472b7fb6765e26
mayank singh thakur <mayanksingh9424@gmail.com>
2026-07-06 21:40:19 +0530

    Implement smallest divisor function with binary search

---
+++ 1283. Find the Smallest Divisor Given a Threshold.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int smallestDivisor(int[] nums, int threshold) {
+
+        int left = 1;
+        int right = 0;
+
+        for (int num : nums) {
+            right = Math.max(right, num);
+        }
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (canDivide(nums, threshold, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    public boolean canDivide(int[] nums, int threshold, int divisor) {
+
+        int sum = 0;
+
+        for (int num : nums) {
+
+            sum += (num + divisor - 1) / divisor;
+
+        }
+
+        return sum <= threshold;
+    }
+}
