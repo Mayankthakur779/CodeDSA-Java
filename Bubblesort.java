c8edb93fc9035b66c95c91f113a99adcd6ad3662
mayank singh thakur <mayanksingh9424@gmail.com>
2026-07-12 23:55:25 +0530

    Refactor Bubblesort class and methods
    Refactored Bubblesort class to improve readability and functionality.

--- Bubblesort.java
+++ Bubblesort.java
@@ -1,22 +1,31 @@
-import java.util.*;
-class Bubblesort{
-    public static void printarr(int arr[]){
-        for(int i=0;i<arr.length;i++){
-            System.out.println(arr[i]+" ");
-        }
+
+public class Bubblesort{
+public static void print(int []arr){
+    for (int ele: arr){
+
+        System.out.print(ele+" ");
+
     }
-    public static void main(String args[]){
-        int arr[]={5,3,4,1,2};
-        //bubble sort
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-i-1;j++){
-                if(arr[j]>arr[j+1]){
-                    int temp=arr[j]; //swap
-                    arr[j]=arr[j+1];
-                    arr[j+1]=temp;
-                }
+    System.out.println();
+}
+
+public static void bubble(int []arr){
+    int n= arr.length;
+    for (int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
             }
         }
-        printarr(arr);
+    }
+}
+
+
+public static void main(String args[]){
+    int arr[]={56,32,12,45,67,89,23};
+    bubble(arr);
+    print(arr);
     }
 }
