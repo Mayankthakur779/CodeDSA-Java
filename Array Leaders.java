4e4254d57a3efe0a7f4eed732b3808518e7518a0
mayank singh thakur <mayanksingh9424@gmail.com>
2026-07-23 23:57:39 +0530

    Create Array Leaders.java

---
+++ Array Leaders.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+
+        ArrayList<Integer> ans = new ArrayList<>();
+
+        int n = arr.length;
+
+        int max = arr[n - 1];
+        ans.add(max);
+
+        for (int i = n - 2; i >= 0; i--) {
+
+            if (arr[i] >= max) {
+                max = arr[i];
+                ans.add(max);
+            }
+        }
+
+        Collections.reverse(ans);
+
+        return ans;
+    }
+}
