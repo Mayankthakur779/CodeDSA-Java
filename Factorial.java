e345cceb7b9c2b0c01c917b4ef65cc01c4d48119
mayank singh thakur <mayanksingh9424@gmail.com>
2026-07-26 23:33:30 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,16 @@
+import java.util.*
+class Factorial{
+    public static void main(String args[]){
+        Scanner sc =new Scanner(Sytsem.in);
+        System.out.print("Enter the number: ");
+        int n=sc.nextInt;
+
+        print()
+    }
+    public Static int fact (int n){
+        if(n==0 || n==1) return 1;
+        int ans=n*fact(n-1);
+        return ans;
+        
+    }
+}
