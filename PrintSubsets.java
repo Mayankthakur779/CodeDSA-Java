001bae0e6811ab38ac7fa6405d0ec7487f791ca5
mayank singh thakur <mayanksingh9424@gmail.com>
2026-08-23 10:33:49 +0530

    Add PrintSubsets class to generate subsets of a string

---
+++ PrintSubsets.java
@@ -0,0 +1,16 @@
+
+public class PrintSubsets{
+    public static void main(String[] args){
+        String s="abc";
+        subsets("",s,0);
+    }
+    public static void subsets(String ans,String s,int idx){
+        if (idx==s.length()) {
+            System.out.print(ans+" ");
+            return;
+        }
+        char ch=s.charAt(idx);             
+        subsets(ans+ch,s,idx+1); //pick
+        subsets(ans,s,idx+1); //skip
+    }
+}
